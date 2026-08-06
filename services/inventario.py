79e8cc2533e73172d90927fbffa2430ebbfcfc60
Meridel <meridelcalderon18@gmail.com>
2026-08-05 20:04:45 -0500

    Modelo de Venta

---
+++ services/inventario.py
@@ -0,0 +1,21 @@
+class Inventario:
+    def __init__(self):
+        """
+        Inventario en estructura de lista, orden e insercion
+        """
+        self.productos = []
+        
+    def registrar_producto(self):
+        """
+        Registra un producto en el inventario.
+        """
+        print("\n[Inventario]")
+        print("Registrar producto pendiente")
+        
+    def listar_productos(self):
+        print("\n[Inventario]")
+        print("Listar productos pendiente")
+        
+    def buscar_producto(self):
+        print("\n[Inventario]")
+        print("Buscar producto pendiente")
