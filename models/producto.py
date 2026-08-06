5d4d969d68f6db98a86e2f23e6932a8a0c06c319
Meridel <meridelcalderon18@gmail.com>
2026-08-05 19:58:50 -0500

    Menu del usuarios

---
+++ models/producto.py
@@ -0,0 +1,30 @@
+class Producto:
+    def __init__(self):
+        """
+        Contructor de la clase Producto.
+        Este metodo recibira los atributos de la clase Producto y los inicializara.
+        """
+        
+    def mostrar(self):
+        """
+        Muestra la informacion del producto.
+        """
+        print("Metodo mostrar() pendiente de implementar.")
+        
+    def aumentar_stock(self):
+        """
+        Aumenta el stock del producto.
+        """
+        print("Metodo aumentar_stock() pendiente de implementar.")
+        
+    def disminuir_stock(self):
+        """
+        Disminuye el stock del producto.
+        """
+        print("Metodo disminuir_stock() pendiente de implementar.")
+        
+    def hay_stock(self):
+        """
+        Verifica si hay stock del producto.
+        """
+        print("Metodo hay_stock() pendiente de implementar.")
---
+++ models/venta.py
@@ -0,0 +1,18 @@
+class Venta:
+    def __init__(self):
+        """
+        Contructor de la clase Venta.
+        """
+        pass
+    
+    def calcular_total(self):
+        """
+        Calcula el total de la venta.
+        """
+        print("Metodo calcular_total() de la clase Venta.")
+        
+    def mostrar(self):
+        """
+        Muestra la informacion de la venta.
+        """
+        print("Metodo mostrar() de la clase Venta.")
